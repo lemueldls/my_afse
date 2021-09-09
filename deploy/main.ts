e67d6833a6949532efff4f0f22f4701c811a53aa
Lemuel DLS <lemueld73@gmail.com>
2021-09-09 15:24:58 -0400

    🚀 Fix deploy

--- deploy/main.ts
+++ deploy/main.ts
@@ -2,22 +2,29 @@
 
 const token = Deno.env.get("TOKEN");
 
-if (token)
-  addEventListener("fetch", async (event) => {
-    const data = await fetch(
-      "https://api.github.com/repos/lemueld6200/my_afse/releases/latest",
-      { headers: { Authorization: token } }
-    );
+addEventListener("fetch", async (event) => {
+  if (!token) return event.respondWith(new Response("Invalid token"));
 
-    const { tag_name, assets } = await data.json();
+  const { pathname } = new URL(event.request.url);
 
-    event.respondWith(
-      new Response(
-        JSON.stringify({
-          version: tag_name.substring(1),
-          url: assets[0].browser_download_url,
-        }),
-        { headers: { accept: "application/json" } }
-      )
-    );
-  });
+  const file = pathname.substring(1);
+
+  const headers = new Headers({ Authorization: token });
+
+  const data = await fetch(
+    "https://api.github.com/repos/lemueld6200/my_afse/releases/latest",
+    { headers }
+  );
+
+  const { tag_name, assets } = await data.json();
+
+  const version = tag_name.substring(1);
+
+  if (!file) return event.respondWith(new Response(version));
+
+  headers.set("accept", "application/octet-stream");
+
+  for (const { name, url } of assets) {
+    if (file === name) return event.respondWith(await fetch(url, { headers }));
+  }
+});
