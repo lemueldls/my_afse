ac66ac781a86d6d46f945f7b9328825c1e83ef53
Lemuel DLS <lemueld73@gmail.com>
2021-09-09 12:21:51 -0400

    🚀 Update deploy

--- deploy/main.ts
+++ deploy/main.ts
@@ -2,16 +2,22 @@
 
 const token = Deno.env.get("TOKEN");
 
-console.log(token);
-
 if (token)
   addEventListener("fetch", async (event) => {
+    const data = await fetch(
+      "https://api.github.com/repos/lemueld6200/my_afse/releases/latest",
+      { headers: { Authorization: token } }
+    );
+
+    const { tag_name, assets } = await data.json();
+
     event.respondWith(
-      await fetch(
-        "https://api.github.com/repos/lemueld6200/my_afse/releases/latest",
-        {
-          headers: { Authorization: token },
-        }
+      new Response(
+        JSON.stringify({
+          version: tag_name.substring(1),
+          url: assets[0].browser_download_url,
+        }),
+        { headers: { accept: "application/json" } }
       )
     );
   });
